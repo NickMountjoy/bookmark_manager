2690fa7fdfcdeff2772a5ab2681e52dfe8e950af
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-24 15:47:33 +0000

    19 Password confirmation added

--- app/app.rb
+++ app/app.rb
@@ -12,53 +12,54 @@ class Bookmark < Sinatra::Base
     def current_user
      @current_user ||= User.get(session[:user_id])
     end
-end
-
-get '/' do
-  'Hello Bookmark!'
-end
-
-get '/links' do
-  @links = Link.all
-  erb :'links/index'
-end
-
-get '/links/new' do
-  erb :'links/new'
-end
-
-post '/links' do
-  link = Link.create(title: params[:title], url: params[:url])
-  params[:tags].split.each do |tag|
-    link.tags << Tag.create(name: tag)
   end
-  #tag = Tag.create(name: params[:tags])
-  #link.tags << tag
-  link.save
-  redirect to('/links')
-end
 
-get '/tags/:name' do
-  tag = Tag.first(name: params[:name])
-  @links = tag ? tag.links : []
-  erb :'links/index'
-end
+  get '/' do
+    'Hello Bookmark!'
+  end
 
-get '/users/new' do
-    erb :'users/new'
-end
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
 
-post '/users' do
-  user = User.create(email: params[:email],
-              password: params[:password])
-  session[:user_id] = user.id
-  redirect to('/links')
-end
+  get '/links/new' do
+    erb :'links/new'
+  end
 
-get '/links/tags' do
-  @tags = Tag.all
-  erb :'links/tags'
-end
+  post '/links' do
+    link = Link.create(title: params[:title], url: params[:url])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
+    #tag = Tag.create(name: params[:tags])
+    #link.tags << tag
+    link.save
+    redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
+  get '/users/new' do
+      erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  get '/links/tags' do
+    @tags = Tag.all
+    erb :'links/tags'
+  end
 
 
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,10 @@
 feature 'User sign up' do
+
+  scenario 'Requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, joe@example.com')
@@ -6,10 +12,12 @@ feature 'User sign up' do
   end
 end
 
-def sign_up
+def sign_up(email: 'joe@example.com',
+            password: '12345678',
+            password_confirmation: '12345678')
   visit '/users/new'
-  expect(page.status_code).to eq(200)
-  fill_in :email,    with: 'joe@example.com'
-  fill_in :password, with: 'oranges!'
+  fill_in :email,                 with: email
+  fill_in :password,              with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
